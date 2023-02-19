fa80d7bc02e95e72e01815acc79c8878ed37bd7d
sinchu2304 <79959648+sinchu2304@users.noreply.github.com>
2023-02-19 11:14:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout<<"Small change by PES1UG20CS630/n";
   return 0;
 }
